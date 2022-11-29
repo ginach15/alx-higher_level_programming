2b467d9cf0db1b5c0e15b9c87663b6a2fab75abd
ginach15 <ginach1215@gmail.com>
2022-11-29 10:01:29 -0800

    task13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,40 +1,47 @@
-#include "lists.h"
 #include <stdlib.h>
-/**
- * createNode - create a new node
- * @prmNbr: number
- * Return: new element
- */
-listint_t *createNode(const int prmNbr)
-{
-	listint_t *new;
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = prmNbr;
-	return (new);
-}
+
+#include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the beginning of a list_t list
- * @head: pointer to last element
- * @n: number
- * Return: new last element
+ * insert_node - inserts a node into a linked list
+ * @head: pointer to pointer to beginning of linked list
+ * @number: integer to be entered into new node
+ * Return: pointer to new node
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new;
-	if (head == NULL)
-		return (NULL);
-	new = createNode(number);
+	listint_t *new, *current, *temp;
+
+	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+	new->n = number;
 	if (*head == NULL)
-		*head = new;
-	else
 	{
-		new->next = *head;
 		*head = new;
+		new->next = NULL;
+		return (new);
 	}
+	current = temp = *head;
+	if (new->n <= current->n)
+	{
+		new->next = current;
+		*head = new;
+		return (new);
+	}
+	while (current->next != NULL)
+	{
+		if (new->n <= current->next->n)
+		{
+			temp = current;
+			current = current->next;
+			temp->next = new;
+			new->next = current;
+			return (new);
+		}
+		current = current->next;
+	}
+	current->next = new;
+	new->next = NULL;
 	return (new);
 }
