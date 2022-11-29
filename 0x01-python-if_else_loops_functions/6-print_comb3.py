0a4ccbdaab749b53f6652053063ba3c459ed37f8
ginach15 <ginach1215@gmail.com>
2022-11-29 08:34:55 -0800

    task6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 99):
- if i % 10 != i / 10 and i % 10 > i / 10:
-    print("{:02d}".format(i), end='')
- if (i / 10 < 8):
-    print(", ", end="")
-    print()
+for dig1 in range(0, 9):
+    for dig2 in range(1, 10):
+        if ((dig1 == 8) and (dig2 == 9)):
+            print("{}{}".format(dig1, dig2))
+        elif (dig1 < dig2):
+            print("{}{}, ".format(dig1, dig2), end='')
