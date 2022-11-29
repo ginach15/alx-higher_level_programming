5647654517c35da1c43b55cd8498f27ddf062f07
ginach15 <ginach1215@gmail.com>
2022-11-29 10:04:59 -0800

    task14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,5 +2,5 @@
 for c in reversed(range(ord('a'), ord('z') + 1)):
         if c % 2 != 0:
                     print("{:c}".format(c - 32), end='')
-                        else:
+        else:
                                     print("{:c}".format(c), end='')
