609b7984e1deafcfda0411b9ac3f9b9c27d82b00
ginach15 <ginach1215@gmail.com>
2022-11-29 08:49:23 -0800

    task9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-        if (number < 0):
-                    number *= -1
-                        print(number % 10, end='')
-                            return (number % 10)
+    if (number < 0):
+        number *= -1
+    else:
+
+        num2 = number
+        lastdigit = num2 % 10
+        print(number % 10, end='')
+        return (number % 10)
