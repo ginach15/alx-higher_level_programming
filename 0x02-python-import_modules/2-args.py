b8ab3fb0bb6313a34157b6ad4acb6b4781012672
ginach15 <ginach1215@gmail.com>
2022-12-01 07:34:47 -0800

    task9

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
         print("{:d} arguments:".format(len(sys.argv) - 1))
     if len(sys.argv) != 1:
         for i in range(1, len(sys.argv)):
-         print("{:d}: {}".format(i, sys.argv[i]))
+            print("{:d}: {}".format(i, sys.argv[i]))
