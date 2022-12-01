654e9785d87187f878c17d16712d54bd959a275e
ginach15 <ginach1215@gmail.com>
2022-12-01 05:57:10 -0800

    task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1 as calc
+    import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.".format())
+    elif len(sys.argv) == 2:
+        print("1 argument:".format())
+    else:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+if len(sys.argv) != 1:
+    for i in range(1, len(sys.argv)):
+         print("{:d}: {}".format(i, sys.argv[i]))
