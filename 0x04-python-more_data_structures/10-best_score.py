582cd18145986716b2542cd7faeb35ad43f53aa5
ginach15 <ginach1215@gmail.com>
2022-12-07 05:21:54 -0800

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if (a_dictionary is None or len(a_dictionary) == 0):
         return None
-             k = list(a_dictionary.keys())
-    o = sorted(a_dictionary.values())
-    v = list(a_dictionary.values())
-    vhi = o[-1]
-    for no in range(len(v)):
-        if v[no] == vhi:
-            khi = k[no]
-    return khi
+    return max(a_dictionary, key=a_dictionary.get)
