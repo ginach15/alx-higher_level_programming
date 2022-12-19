f60911474d763fabf5404b6b852256ef42e66c34
ginach15 <ginach1215@gmail.com>
2022-12-19 10:22:25 -0800

    Exceptions

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    newlist = []
+
+    for i in range(0, list_length):
+        num = 0
+
+        try:
+            num = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("{}".format("division by 0"))
+        except TypeError:
+            print("{}".format("wrong type"))
+        except IndexError:
+            print("{}".format("out of range"))
+        finally:
+            newlist.append(num)
+            return newlist
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,10 @@
+python - Exceptions
+
+Safe list printing
+Safe printing of an integers list
+Print and count integers
+Integers division with debug
+Divide a list
+Raise exception
+Raise a message
+
