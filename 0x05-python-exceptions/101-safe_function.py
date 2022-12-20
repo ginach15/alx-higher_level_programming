87012436168229104a70e67087915b9f5e845bd5
ginach15 <ginach1215@gmail.com>
2022-12-19 16:31:08 -0800

    magic_calculation and safe calculation

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as exception:
+        sys.stderr.write("Exception: ")
+        sys.stderr.write(exception.args[0])
+        sys.stderr.write('\n')
