1d33d00e8c6fc601c75c7c1623598238835a8500
ginach15 <ginach1215@gmail.com>
2022-12-19 16:11:50 -0800

    6-raise_exception_msg.py

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-        print("{}".format(message), end="")
-        raise NameError
+    print("{}".format(message), end="")
+    raise NameError
