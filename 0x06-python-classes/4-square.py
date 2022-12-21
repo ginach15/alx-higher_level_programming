b4c71baa6d8c4055949f2c24c884d6ee4be57b46
ginach15 <ginach1215@gmail.com>
2022-12-20 17:23:38 -0800

    4-squre

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""This module makes a class Square and has a private instance attribute size.
+"""
+
 class Square:
+    """a square is a parallelogram
+    Attributes:
+    __size: instantiates Square with size
+    size must be an integer that is 0 or greater
+    """
+
     def __init__(self, size=0):
+        """this function instantiates with size private instance attribute
+        """
+
         if not isinstance(size, int):
             print("size must be an integer", end="")
             raise TypeError
@@ -9,18 +21,9 @@ class Square:
             raise ValueError
         else:
             self.__size = size
-            def area(self):
+
+    def area(self):
+        """this function returns the area of the square
+        """
+
                 return self.__size ** 2
-            @property
-            def size(self):
-                return self.__size
-            @size.setter
-            def size(self, value):
-                if not isinstance(value, int):
-                    print("size must be an integer", end="")
-                    raise TypeError
-                elif value < 0:
-                    print("size must be >= 0", end="")
-                    raise ValueError
-                else:
-                    self.__size = value
