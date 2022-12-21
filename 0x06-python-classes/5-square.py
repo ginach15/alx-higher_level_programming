44235252c4516d5a213ac2c9ec72fbadd541cca5
ginach15 <ginach1215@gmail.com>
2022-12-21 00:54:44 -0800

    5-square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -20,41 +20,41 @@ class Square:
 
         self.size = size
 
-        def area(self):
-            """calculates the square's area
+    def area(self):
+        """calculates the square's area
 
-            Returns:
-                The area of the square
-            """
+        Returns:
+            The area of the square
+        """
 
-            return (self.__size) ** 2
-        @property
-        def size(self):
-            """getter of __size
+        return (self.__size) ** 2
+    @property
+    def size(self):
+        """getter of __size
 
-            Returns:
-                The size of the square
-            """
+        Returns:
+            The size of the square
+        """
 
-            return self.__size
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            """setter of __size
+    @size.setter
+    def size(self, value):
+        """setter of __size
 
-            Args:
-                value (int): size of a side of the square
-            Returns:
-                None
-            """
+        Args:
+            value (int): size of a side of the square
+        Returns:
+            None
+        """
 
-            if type(value) is not int:
-                raise TypeError("size must be an integer")
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
             else:
-                if value < 0:
-                    raise ValueError("size must be >= 0")
-                else:
-                    self.__size = value
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
     def my_print(self):
         """prints the square
 
