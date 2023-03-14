22d0f21d98a5eac2ddc0e2f57434b688d744dd8e
ginach15 <ginach1215@gmail.com>
2023-03-14 00:51:56 -0700

    13-add.js

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,10 +1,3 @@
 #!/usr/bin/node
-/**
- * Add two numbers
- * @param {*} a first number
- * @param {*} b second number
- * @returns result of addition of two parameters
- */
- exports.add = function (a, b) {
-  return (a + b);
-}
+// returns the addition of 2 integers
+exports.add = function (a, b) { return (a + b); };
