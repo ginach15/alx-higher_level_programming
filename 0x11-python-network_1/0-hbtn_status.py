388151eae309540406e3026930899b8cba0d2eae
ginach15 <ginach1215@gmail.com>
2023-03-31 04:12:21 -0700

    network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status """
-import urllib.request
 
+""" Python script that fetches https://intranet.hbtn.io/status """
+
+import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
@@ -10,4 +11,3 @@ if __name__ == "__main__":
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
         print('\t- utf8 content: {}'.format(html.decode("utf-8"))
-
